7ad8a56966875f3beb132c068042a5722bc09cff
Thalida Noel <thalida@tourus.io>
2024-06-21 14:31:18 -0400

    Update install-pg-dump.sh

--- install-pg-dump.sh
+++ install-pg-dump.sh
@@ -14,12 +14,12 @@ if [[ "$POSTGRES_VERSION" != @(11|12|13|14|15|16) ]]; then
 fi
 
 [[ $POSTGRES_VERSION == 16 ]] \
-    && POSTGRES_DOWNLOAD_URL="http://download.postgresql.org/pub/repos/yum/$POSTGRES_VERSION/redhat/rhel-7.0-x86_64" \
+    && POSTGRES_DOWNLOAD_URL="http://download.postgresql.org/pub/repos/yum/$POSTGRES_VERSION/redhat/rhel-8-x86_64" \
     || POSTGRES_DOWNLOAD_URL="http://download.postgresql.org/pub/repos/yum/$POSTGRES_VERSION/redhat/rhel-7-x86_64"
 
 cat <<EOF > /etc/yum.repos.d/pgdg.repo
 [pgdg$POSTGRES_VERSION]
-name=PostgreSQL $POSTGRES_VERSION for RHEL/CentOS 7 - x86_64
+name=PostgreSQL $POSTGRES_VERSION for RHEL/CentOS - x86_64
 baseurl=$POSTGRES_DOWNLOAD_URL
 enabled=1
 gpgcheck=0
